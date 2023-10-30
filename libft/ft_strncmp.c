58005b84f425f1c0b72f101afba30994e2a1eae9
sgo <ko9603@naver.com>
2023-10-31 01:39:05 +0900

    [delete] : 오브젝트 파일들 삭제

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seoson <seoson@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: sgo <sgo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 11:28:03 by seoson            #+#    #+#             */
-/*   Updated: 2023/04/13 14:26:22 by seoson           ###   ########.fr       */
+/*   Updated: 2023/10/29 16:51:07 by sgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (i < n && s1[i] && s2[i])
+	while (i < n)
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
