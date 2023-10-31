9d8e4f56465f369f4b32ff3454c98b3c6333e7e1
sgo <ko9603@naver.com>
2023-10-31 12:51:48 +0900

    [feat]: wait 순서 변경, cmd print함수 추가

--- executor/executor.c
+++ executor/executor.c
@@ -6,11 +6,25 @@
 /*   By: sgo <sgo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 11:26:46 by sgo               #+#    #+#             */
-/*   Updated: 2023/10/31 01:31:29 by sgo              ###   ########.fr       */
+/*   Updated: 2023/10/31 12:49:06 by sgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
+
+void	print_cmd(t_cmd *cmd)
+{
+	int	i;
+
+	i = 0;
+	while (cmd->cmd[i])
+	{
+		printf("cmd[%d]: %s\n", i, cmd->cmd[i]);
+		i++;
+	}
+	printf("cmd->str: %s\n", cmd->redir->str);
+	printf("cmd->file_name: %s\n", cmd->redir->filename);
+}
 
 int	executor(t_cmd *cmd, t_envp *envp)
 {
@@ -19,6 +33,7 @@ int	executor(t_cmd *cmd, t_envp *envp)
 	init_info(&info, envp);
 	while (cmd)
 	{
+		// print_cmd(cmd);
 		file_open(cmd, &info);
 		if (is_builtin(cmd->cmd[0]))
 			builtin(cmd, &info, envp);
--- executor/wait.c
+++ executor/wait.c
@@ -6,7 +6,7 @@
 /*   By: sgo <sgo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 00:36:55 by sgo               #+#    #+#             */
-/*   Updated: 2023/10/31 00:44:59 by sgo              ###   ########.fr       */
+/*   Updated: 2023/10/31 10:41:07 by sgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	wait_all(t_info *info)
 	int	count;
 
 	count = 0;
+	waitpid(info->pid, &info->status, 0);
 	while (count < info->cnt - 1)
 	{
 		if (wait(0) == -1)
 			exit(1);
 		count++;
 	}
-	waitpid(info->pid, &info->status, 0);
 }
