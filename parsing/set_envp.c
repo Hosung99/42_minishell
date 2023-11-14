ed3b6d9221dc9cc19942590a350d81160b589525
Seongho Son <seoson@cx2r2s5.42seoul.kr>
2023-11-14 19:10:42 +0900

    [feat]: envp key에 =추가

--- parsing/set_envp.c
+++ parsing/set_envp.c
@@ -6,20 +6,19 @@
 /*   By: seoson <seoson@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 14:20:28 by seoson            #+#    #+#             */
-/*   Updated: 2023/11/14 14:45:11 by seoson           ###   ########.fr       */
+/*   Updated: 2023/11/14 19:10:18 by seoson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	make_envp_cmd(t_envp *envp_list, char **str, int cnt)
+void	make_envp_cmd(t_envp *envp_list, char **str)
 {
 	t_envp	*envp;
 
 	envp = (t_envp *)malloc(sizeof(t_envp));
 	envp->key = ft_strdup(str[0]);
-	if (cnt == 2)
-		envp->value = ft_strdup(str[1]);
+	envp->value = ft_strdup(str[1]);
 	envp->next = NULL;
 	if (envp_list->next == NULL)
 		envp_list->next = envp;
@@ -30,6 +29,34 @@ void	make_envp_cmd(t_envp *envp_list, char **str, int cnt)
 			envp_list = envp_list->next;
 		envp_list->next = envp;
 	}
+}
+
+static int	len_word(char const *s, char c)
+{
+	int	save;
+
+	save = 0;
+	while (*s)
+	{
+		if (*s == c)
+			break ;
+		s++;
+		save++;
+	}
+	return (save);
+}
+
+char	**ft_split_equal(char *s)
+{
+	int		str_index;
+	char	**str;
+
+	str_index = len_word(s, '=');
+	str = (char **)malloc(sizeof(char *) * (3));
+	str[0] = ft_substr(s, 0, str_index + 1);
+	str[1] = ft_substr(s, str_index + 1, ft_strlen(s) - str_index);
+	str[2] = 0;
+	return (str);
 }
 
 void	set_envp(char **envp, t_envp *envp_list)
@@ -43,8 +70,8 @@ void	set_envp(char **envp, t_envp *envp_list)
 	while (envp[envp_index])
 	{
 		split_cnt = 0;
-		split_str = ft_split(envp[envp_index], '=', &split_cnt);
-		make_envp_cmd(envp_list, split_str, split_cnt);
+		split_str = ft_split_equal(envp[envp_index]);
+		make_envp_cmd(envp_list, split_str);
 		free_str(split_str);
 		envp_index++;
 	}
